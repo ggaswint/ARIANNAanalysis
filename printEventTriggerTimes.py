98f91cc1c6c9382d1c9b0873af637a621f7f4b43
Geoffrey <ggaswint@uci.edu>
2021-01-20 16:54:57 -0800

    Added averaging script with plots and a script to print trigger times per event

--- printEventTriggerTimes.py
+++ printEventTriggerTimes.py
@@ -7,17 +7,17 @@ readARIANNAData = CreadARIANNAData.readARIANNAData()
 det = detector.Detector(source='sql',assume_inf=False)  # establish mysql connection
 
 def printTimes(file):
-	n_events = readARIANNAData.begin([file])
-	event_count = 0
-	for evt in readARIANNAData.run():
-		for station_object in evt.get_stations():
-			print_str = 'event: ' + str(event_count) + ' time: ' + str(station_object.get_station_time())
-			print(print_str)
-			event_count += 1
+    n_events = readARIANNAData.begin([file])
+    event_count = 0
+    for evt in readARIANNAData.run():
+        for station_object in evt.get_stations():
+            print_str = 'event: ' + str(event_count) + ' time: ' + str(station_object.get_station_time())
+            print(print_str)
+            event_count += 1
 
 def main():
     file = PathToARIANNAanalysis + '/data/Spice_750mDown_Dec30_2018_idl_10dB.root'
     printTimes(file)
 
 if __name__== "__main__":
-	main()
+    main()
